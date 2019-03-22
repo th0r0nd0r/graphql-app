0aa8601594def160773fe92a8eda4873398f1713
th0r0nd0r <pjfritch1@gmail.com>
2019-03-22 16:19:53 -0700

    update schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,29 @@
 import graphql from 'graphql';
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLString },
     title: { type: GraphQLString },
-    genre: { type: GraphQLString}
+    genre: { type: GraphQLString }
   })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db/other source
+      }
+    }
+  }
+});
+
+export default new GraphQLSchema({
+  query: RootQuery
 });
