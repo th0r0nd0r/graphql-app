569a7ab430ae669f4a17fd253ed328e954a60c3c
th0r0nd0r <pjfritch1@gmail.com>
2019-03-22 20:17:57 -0700

    add mutations

--- server/models/book.js
+++ server/models/book.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  name: String,
+  title: String,
   genre: String,
-  authorId: String
+  authorId: String,
 });
 
 export default mongoose.model('Book', bookSchema);
