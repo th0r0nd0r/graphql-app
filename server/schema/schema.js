2e152ef19266db995f2f127d6f49855375a0c690
th0r0nd0r <pjfritch1@gmail.com>
2019-03-22 18:09:10 -0700

    fix graphql import issue

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
-import graphql from 'graphql';
+import * as graphql from 'graphql';
+import _ from 'lodash';
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+let books = [
+  { title: 'Frumgers Dine at Midnight', genre: 'Biography', id: '1'},
+  { title: 'Spumpy and the Great Albacore', genre: 'Adventure', id: '2'},
+  { title: 'Robots Eat Electric Peeps', genre: 'Sci-Fi', id: '3'},
+];
+
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -19,6 +28,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from db/other source
+        _.find(books, {id: args.id});
       }
     }
   }
